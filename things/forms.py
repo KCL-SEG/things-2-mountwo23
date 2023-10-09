a1d8f55a755601d87c9a498439dd7955b6505f47
Baekhan Sung <ksung3012@gmail.com>
2023-10-09 17:40:00 +0100

    Update forms.py

--- things/forms.py
+++ things/forms.py
@@ -13,7 +13,7 @@ class ThingForm(forms.ModelForm):
         model = Thing
         fields = ['name', 'description', 'quantity']
     name = forms.CharField(max_length=35)
-    description = forms.CharField(max_length=120)
+    description = forms.Textarea()
     quantity = forms.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(50)]
     )
