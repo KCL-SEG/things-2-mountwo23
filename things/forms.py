e14d97428cbb47dbe14fd65fdd3334ef90bb20bd
Baekhan Sung <ksung3012@gmail.com>
2023-10-09 17:44:07 +0100

    Fix Form

--- things/forms.py
+++ things/forms.py
@@ -1,19 +1,14 @@
-"""Forms of the project."""
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from things.models import Thing
 
-
-# Create your forms here.
-
 class ThingForm(forms.ModelForm):
     """Form for Thing model."""
+
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
-    name = forms.CharField(max_length=35)
-    description = forms.Textarea()
-    quantity = forms.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(50)]
-    )
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
