c144a3aae0da6f51597649f3546e64a6aa6e0d76
Baekhan Sung <ksung3012@gmail.com>
2023-10-09 17:36:38 +0100

    Add Thing Form and HTML

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,19 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from things.models import Thing
+
 
 # Create your forms here.
+
+class ThingForm(forms.ModelForm):
+    """Form for Thing model."""
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120)
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(50)]
+    )
